52a5f7cb2d4b8b661b2546415735dee666db06c7
gabicfa <gabrielacfa@al.insper.edu.br>
2016-11-30 23:53:00 -0200

    mensagem printada no html

--- server.js
+++ server.js
@@ -19,10 +19,11 @@ io.sockets.on('connection', function(socket){
 
 	socket.on('disconnect', function(data){
 		connections.splice(connections.indexOf(socket),1);
-		console.log('Disconnecte:%s sockets connected', connections.length);	
+		console.log('Disconnecte: %s sockets connected', connections.length);	
 	});
 
 	socket.on('send message', function(data){
+		console.log(data);
 		io.sockets.emit('new message',{msg:data});
 	});
 });
