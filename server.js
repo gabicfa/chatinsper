130f00bbc87e168e4ab381591fddaed369adf81f
gabicfa <gabrielacfa@al.insper.edu.br>
2016-12-01 14:27:24 -0200

    Arrumando pastas

--- server.js
+++ server.js
@@ -9,19 +9,21 @@ connections = [];
 server.listen(process.env.PORT || 3000);
 console.log('Servidor Funcionando')
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req,res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/View/index.html');
 });
 
 io.sockets.on('connection', function(socket){
 	connections.push(socket);
-	console.log('Conneted: %s sockets Conneted', connections.length);
+	// console.log('Conneted: %s sockets Conneted', connections.length);
 
 	socket.on('disconnect', function(data){
 		users.splice(users.indexOf(socket.username),1);
 		updateUsernames();
 		connections.splice(connections.indexOf(socket),1);
-		console.log('Disconnecte: %s sockets connected', connections.length);	
+		// console.log('Disconnecte: %s sockets connected', connections.length);	
 	});
 
 	socket.on('send message', function(data){
